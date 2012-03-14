0bc41cd56d4f80c316222aa5f6800f70b46493ab
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2012-03-14 23:39:12 +0100

    Making CLI nicer

--- hncli/main.py
+++ hncli/main.py
@@ -17,6 +17,14 @@ class HackerNews(cmd.Cmd):
 		cmd.Cmd.__init__(self, *args, **kwargs)	# cmd.Cmd is old-style class!
 		self.stories = {}
 
+	def _help(self, command):
+		''' Returns the help text for given command. '''
+		method = getattr(self, 'do_' + command, None)
+		if not method:
+			return None
+		help = method.__doc__.strip()
+		return re.sub('\n\\s+', '\n', help)	# collapse some whitespace
+
 	def _get_stories(self, count, page):
 		if not count or len(str(count).strip()) == 0:
 			count = 10
@@ -57,16 +65,39 @@ class HackerNews(cmd.Cmd):
 		else:
 			print "*** Unkown story: %s" % story
 
+	def do_help(self, command):
+		''' Display help for given command. '''
+		if command:
+			help = self._help(command.strip())
+			if help:
+				print help
+				return
+		cmd.Cmd.do_help(self, command)
+
 	def default(self, command):
 		if command in ['exit', 'quit']:
 			sys.exit()
 		else:
 			cmd.Cmd.default(self, command)
 
+	def emptyline(self):
+		pass # do nothing (and don't repeat last command)
+
 
 def main():
 	hncli = HackerNews()
+	hncli.intro = "\n".join([
+		"hncli :: command-line interface for Hacker News",
+		"[Python %s on %s]" % (sys.version.splitlines()[0], sys.platform),
+		"Use 'help' or type 'top' for front page stories.",
+	])
 	hncli.prompt = "hn$ "
+
+	hncli.doc_header = "Supported commands"
+	hncli.undoc_header = "Other commands"
+	hncli.misc_header = "Help topics"
+	hncli.ruler = "*"
+
 	hncli.cmdloop()
 
 
